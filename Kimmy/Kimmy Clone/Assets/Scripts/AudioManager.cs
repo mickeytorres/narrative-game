036609a235c7a2ea78ccc57c0c9e6e8d8a812e3a
Mickey Torres <met424@nyu.edu>
2018-12-02 23:31:56 -0500

    kimmy 12.2.18, fancy text

---
+++ Kimmy/Kimmy Clone/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+	public AudioSource Click;
+
+	public void PlayClick()
+	{
+		Click.Play();
+	}
+}
